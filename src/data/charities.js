ba8992c3350eb5788dd4de4d88ebe9e5e80db73b
davaardi <davaas>
2022-01-27 09:42:09 +0700

    Convert HTML to svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://id.pinterest.com/niadestiyanti5/_saved/',
+            pledged: 0,
+            target: 100000,
+            data_end: new Date('10 August 2020'),
+            profile_photo: 'https://id.pinterest.com/pin/777504323172351292/',
+            profile_name: 'Dapdap',
+            no_pledges: 0
+        }
+    ]
+}
